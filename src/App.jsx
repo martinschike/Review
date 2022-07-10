dab72d7fbeab22d11ee986541267ab87f92b2d8b
MARTINS OKAFOR <okaformartinschinwike@gmail.com>
2022-07-10 14:57:42 +0100

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Review from "./Review";
+
+
+const App = () => {
+    return ( 
+<section>
+    <div className="container">
+        <div className="title">
+            <h1>Our Reviews</h1>
+            <div className="underline"></div>
+            <Review />
+
+        </div>
+    </div>
+</section>
+)}
+
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,58 @@
+const reviews = [
+    {
+        id: 1,
+        name: "Chinelo Okafor",
+        job: "Data Analyst",
+        age: 32,
+        image: "https://scontent.fbhx1-1.fna.fbcdn.net/v/t39.30808-6/289561622_5482056088518669_766687586057709702_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=bw5ltVosIpUAX9DKKOI&_nc_ht=scontent.fbhx1-1.fna&oh=00_AT9uETzF-_JIBCUQxPNlbz2HLF__vGrl7n4KejYstbUy1g&oe=62CEA90F",
+        text: "Chinelo is one of the best in her field. She has a Masters degree from the university of Wolverhampton and presently works for Google.",
+    },
+
+    {
+        id: 2,
+        name: "Martins Okafor",
+        job: "Software Engineer",
+        age: 33,
+        image: "https://scontent.fbhx1-1.fna.fbcdn.net/v/t39.30808-6/286496591_10227669168344212_1320737560347712187_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=0an9llqgxOkAX8OY7oa&_nc_ht=scontent.fbhx1-1.fna&oh=00_AT-aNgQ9W29EIF1clC-RdtjtrYT-8nzaaYPluJo0oGZDGg&oe=62CF828D",
+        text: "Martins is one of the best in his field. He has an MBA from the university of East London and presently works for Microsoft.",
+    },
+
+    {
+        id: 3,
+        name: "Emmanuel Okafor",
+        job: "Software Engineer",
+        age: 31,
+        image: "https://scontent.fbhx1-1.fna.fbcdn.net/v/t1.6435-9/184840807_2834077423535064_5234561035661968529_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=174925&_nc_ohc=H_4t6FJ8eTEAX-JfCBC&_nc_ht=scontent.fbhx1-1.fna&oh=00_AT8GZJDit4JYjg1Daa5aCbfM3yTaTUhbrXjsPi2-7BdvzA&oe=62F0BE5C",
+        text: "Emmanuel is one of the best in his field. He has a Masters degree MIT and is the founder and CEO of Voomsway.",
+    },
+
+    {
+        id: 4,
+        name: "Nkiru Chibueze",
+        job: "Customer Service",
+        age: 28,
+        image: "https://scontent.fbhx1-1.fna.fbcdn.net/v/t39.30808-6/277774322_3077473609181072_7739105672845521476_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=J5zyoxZp6S4AX-ggcGT&_nc_ht=scontent.fbhx1-1.fna&oh=00_AT_09B1t-2YSvaDw7qH21ojCNHqVW1T6v9ow2GI4IIP2lQ&oe=62CFE294",
+        text: "Nkiru is one of the best in her field. She has a Masters degree from the university of Lagos and presently works for Flutterwave.",
+    },
+
+    {
+        id: 5,
+        name: "Felix Okafor",
+        job: "Accountant",
+        age: 30,
+        image: "https://scontent.fbhx1-1.fna.fbcdn.net/v/t39.30808-6/289012752_6050994531598560_3999683425461158707_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=n3WPPomV_a0AX-yAhru&_nc_ht=scontent.fbhx1-1.fna&oh=00_AT8BuRBTInNMzKnBIiY6DFqqkdUBmA37topMZVeP1A9r2w&oe=62CEF230",
+        text: "Felix is one of the best in his field. He has a Masters degree from the university of Lagos and presently works for Google.",
+    },
+
+    {
+        id: 6,
+        name: "Chinenye Chibueze",
+        job: "Make-up Artist",
+        age: 21,
+        image: "https://scontent.fbhx1-1.fna.fbcdn.net/v/t1.6435-9/120298555_814518029304209_7659824081571497355_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=WZnEMmfH6s4AX9-DT24&_nc_oc=AQlfnmWIAuonCbZzcvf0o9g4ncpCaSaNtFeycp_3vLrbUE0OYoXGfOb5hjjTQDr8uGY&_nc_ht=scontent.fbhx1-1.fna&oh=00_AT8s-EDUDrZgQ7eDN49HdzuBriHHhKIl2-dkAF914ab_wg&oe=62EF174B",
+        text: "Nenye is one of the best in her field. She has a degree from the University of Uyo and presently owns her ow company.",
+    },
+];
+
+
+export default reviews;
